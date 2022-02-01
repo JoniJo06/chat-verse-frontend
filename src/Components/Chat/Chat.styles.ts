864e3e39d3ab8886f4386efe7d861d7e78f8928c
Tobias Wutz <tobiaswutz22@gmail.com>
2022-02-01 15:53:39 +0100

    textfield

--- src/Components/Chat/Chat.styles.ts
+++ src/Components/Chat/Chat.styles.ts
@@ -3,5 +3,15 @@ import { Paper } from '@mui/material';
 
 export const Wrapper = styled(Paper)({
   width: '50vw',
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: '1',
+  justifyContent: 'flex-end',
+
+  form: {
+    width: '100%',
+    padding: '1rem'
+  }
 });
+
 
